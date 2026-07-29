383b0267d291dbee4811ad38a2b304c967295998
Yaroslav Kuzmenko <yaroslav.kuzmenko@clicktrip.com>
2026-07-29 11:53:51 +0300

    master path and ip for pi

--- HomeWatcher/Publish.ps1
+++ HomeWatcher/Publish.ps1
@@ -1,3 +1,3 @@
-﻿dotnet publish HomeWatcher.csproj -r linux-arm64 --configuration Release --output <project_path>\bin\publish\
+﻿dotnet publish HomeWatcher.csproj -r linux-arm64 --configuration Release --output D:\External\RasberryPiHome\HomeWatcher\bin\publish\
 #first copy with whole dependencies
-pscp -pw <password> -r <project_path>\bin\publish\ pi@<ipaddress>:/home/pi/test/home_watcher/
+pscp -pw admin1820136 -r D:\External\RasberryPiHome\HomeWatcher\bin\publish\HomeWatcher* pi@192.168.50.86:/home/pi/test/home_watcher/
